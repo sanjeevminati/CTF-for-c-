1aab28c5e9612d91fe2ae0a0ab51e7560c5e34b8
sanjeevminati <43167327+sanjeevminati@users.noreply.github.com>
2019-08-24 10:21:36 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void main() {
+	char a[21];
+	int b;
+	int i;
+	printf ("eneter the username:=\n");
+	gets(a);
+    printf("the username is %s\n",a);
+    
+    printf("enetr the pass id\n");
+    scanf("%d",&b);
+    printf("the id is %d\n",b);
+    
+	if ((strcmp(a,"Admin")==0)&& b==897){
+    	printf("ACCESS GRANTED\n");
+    	
+	}
+	else{
+		printf ("This is for admin motherfucker\n");
+	}
+	
+	
+}
+
